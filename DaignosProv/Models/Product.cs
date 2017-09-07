8837c7f9b9361f73023fa976135b661c1b974c35
Khalid <Khalid@DESKTOP-8LLHGDQ>
2017-09-07 09:57:33 +0200

    Validation for Name and Price  in product model

--- DaignosProv/Models/Product.cs
+++ DaignosProv/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace DaignosProv.Models
 {
     public class Product
     {
+        
         public int ProductId { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Range (typeof(decimal),"5","1000")]
         public decimal Priice { get; set; }
 
     }
