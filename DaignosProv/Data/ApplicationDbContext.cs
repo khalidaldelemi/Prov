61a277ee56fe22535ba84f085c9991f4e1115f12
Khalid <Khalid@DESKTOP-8LLHGDQ>
2017-09-07 09:52:30 +0200

    Creat a Dbset to Product

--- DaignosProv/Data/ApplicationDbContext.cs
+++ DaignosProv/Data/ApplicationDbContext.cs
@@ -22,5 +22,6 @@ namespace DaignosProv.Data
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
+        public DbSet<Product> Products { get; set; }
     }
 }
